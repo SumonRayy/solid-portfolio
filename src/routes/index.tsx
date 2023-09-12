84c1a3af141c781676c41bcb6c74bff4aafb700f
SumonRayy <sumonrayy@gmail.com>
2023-09-12 11:48:43 +0530

    🎨 style : some style changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main>
       <Title>Hello World</Title>
       <h1>{import.meta.env.VITE_RESUME_NAME || "Hello World!"}</h1>
-      <p class="intro">
+      <p>
         {import.meta.env.VITE_RESUME_INTRO ||
           "This is a bare SolidStart project."}
       </p>
